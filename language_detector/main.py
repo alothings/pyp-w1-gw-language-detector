1cc74f7073c502b3c7e660bbb280705986a1699f
Alonso Gutierrez <uriel@seas.upenn.edu>
2016-07-13 00:47:18 +0000

    improved readability

--- language_detector/main.py
+++ language_detector/main.py
@@ -7,17 +7,16 @@ def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
 
     ls_num = []
-    ls_text = text.split()
+    words_text = text.split()
     for lang in languages:
-        ls_num.append(num_of_common(ls_text, lang['common_words']))
-
-    return languages[ls_num.index(max(ls_num))]['name']
+        words_lang = lang['common_words']
+        ls_num.append(num_of_common_words(words_text, words_lang))
+        
+    lang_index = ls_num.index(max(ls_num))
+    return languages[lang_index]['name']
 
                 
-def num_of_common(ls_text, ls_lang):
+def num_of_common_words(ls_text, ls_lang):
     return len([word for word in ls_text if word in ls_lang]);
     
-        
-
-#print(common(['a', 'to', 'be'], ['hello', 'to', 'be']))            
    
