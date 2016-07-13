1abf7d74ba9617c2e3cfee4011db9639692cd836
Alonso Gutierrez <uriel@seas.upenn.edu>
2016-07-13 00:31:19 +0000

    Initial working solution to language detector

--- language_detector/main.py
+++ language_detector/main.py
@@ -5,5 +5,19 @@ from .languages import LANGUAGES
 
 def detect_language(text, languages=LANGUAGES):
     """Returns the detected language of given text."""
-    # implement your solution here
-    pass
+
+    ls_num = []
+    ls_text = text.split()
+    for lang in languages:
+        ls_num.append(num_of_common(ls_text, lang['common_words']))
+
+    return languages[ls_num.index(max(ls_num))]['name']
+
+                
+def num_of_common(ls_text, ls_lang):
+    return len([word for word in ls_text if word in ls_lang]);
+    
+        
+
+#print(common(['a', 'to', 'be'], ['hello', 'to', 'be']))            
+   
